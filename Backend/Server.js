c97ef055a5404f17dd4e595c7e8d6ca8ef467bdd
bhanusvc <nimmalabhanusvc@gmail.com>
2022-12-01 20:44:32 +0530

    second commit

---
+++ Backend/Server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+const Admin = require('./models/AdminModel')
+app.use(express.json())
+
+
+app.get('/',(req,res)=>{
+    res.json('Hiiii')
+})
+app.get("/about", (req,res)=>{
+    res.json('Bhanu')
+})
+
+app.post('/add',async (req,res)=>{
+    const {name, email, age} = req.body
+    try{
+        const admin = await Admin.create({name, email, age})
+        res.status(200).json(admin)
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
+    
+})
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+    // listen for requests
+        app.listen(process.env.PORT, ()=>{
+        console.log('connected to DB & listening on port ', process.env.PORT)
+        })
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
